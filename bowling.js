a142772ccfcd4f17de46653f6e52f80a625e6074
livelikeabel <esung1129@gmail.com>
2018-11-27 14:30:58 +0900

    Set base structure
    - class 만들기
    - 실행 메서드 만들기

---
+++ bowling.js
@@ -0,0 +1,14 @@
+const totalFrame = 10
+
+class BowlingGame {
+  constructor() {
+    this.state = {  }
+  }
+  throwBall() {
+    console.log('run throwBall')
+    //몇 프레임 남았는지 알려주기
+  }
+}
+
+const myGame = new BowlingGame()
+myGame.throwBall('82A900519A')
